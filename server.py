a743ee39a8dbc55ed5b9025c9bc79dfda13069b4
ToutBaiser <115180494+ToutBaiser@users.noreply.github.com>
2022-10-07 22:37:06 +0200

    Change string.ascii.letters to string.printable
    Better key :)

--- server.py
+++ server.py
@@ -6,16 +6,15 @@ from github import Github
 
 # generate password
 def generatePassword():
-    return ''.join(random.choice(string.ascii_letters) for i in range(512))
+    return ''.join(random.choice(string.printable) for i in range(300))
 
 def main():
     while(True):
-        g = Github("secret Key")
+        g = Github("key here")
         repo = g.get_repo("ToutBaiser/keyX")
         contents = repo.get_contents("key.nigger")
         commit = generatePassword()
         repo.update_file(contents.path, "Key Update", commit, contents.sha, branch="main")
         sleep(86400)
- 
 if __name__ == "__main__":
     main()

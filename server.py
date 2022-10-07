6e1fdb0ea57c35cdb1567398788d0f50f52fb60c
ToutBaiser <115180494+ToutBaiser@users.noreply.github.com>
2022-10-07 22:28:36 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,21 @@
+import random,string
+from time import sleep
+import requests
+import os
+from github import Github
+
+# generate password
+def generatePassword():
+    return ''.join(random.choice(string.ascii_letters) for i in range(512))
+
+def main():
+    while(True):
+        g = Github("secret Key")
+        repo = g.get_repo("ToutBaiser/keyX")
+        contents = repo.get_contents("key.nigger")
+        commit = generatePassword()
+        repo.update_file(contents.path, "Key Update", commit, contents.sha, branch="main")
+        sleep(86400)
+ 
+if __name__ == "__main__":
+    main()
